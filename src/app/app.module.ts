f54a3d0b3ba9e06e8c0b15aed06ef929eec12fe4
Brad Brown <Bradley.Brown@Kantar.com>
2018-10-06 15:01:32 -0500

    Structure creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './app/modules/dashboard/dashboard.component';
+import { AdminComponent } from './modules/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
--- src/server/server.js
+++ src/server/server.js
@@ -2,12 +2,15 @@ require("dotenv").config();
 
 var path = require('path');
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 const port = 5000;
 
-app.route('/api/settings').get((req, res) => {
-  res.send('settings!');
+app.route('/api/tournament').get((req, res) => {
+  const file = path.resolve(__dirname, 'data/tournament.json');
+  const data = fs.readFileSync(file, 'utf8');
+  res.send(data);
 });
 
 app.listen(port, function () {
