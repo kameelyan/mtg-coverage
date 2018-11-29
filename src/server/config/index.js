30b765d4b1d62d5ea3f62aa391ab9e02e2286d8b
Brad Brown <Bradley.Brown@Kantar.com>
2018-11-28 19:07:58 -0600

    Back to NRG defaults

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -1,7 +1,7 @@
 var config = {
-    OBSDirectory: null,
-    ImagesDirectory: null,
-    JSONDirectory: null
+    OBSDirectory: 'D:/NRG Stream/OBS/NRG Streaming Dashboard/DataFiles/',
+    ImagesDirectory: 'D:/NRG Stream/OBS/NRG Streaming Dashboard/Images/',
+    JSONDirectory: 'D:/NRG Stream/OBS/NRG Streaming Dashboard/JSON/'
 };
 
 module.exports = {
